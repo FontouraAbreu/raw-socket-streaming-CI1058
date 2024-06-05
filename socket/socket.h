a1a15389fe09212e080d405ffad80d01e26e7c8f
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-06-04 21:11:38 -0300

    [created] scoket handler header file

---
+++ socket/socket.h
@@ -0,0 +1,30 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <if_ether.h>
+
+struct kermit_frame_t {
+    unsigned start;
+    unsigned size;
+    unsigned seq;
+    unsigned type;
+    unsigned data[64];
+    unsigned crc_8;
+} kermit_frame_t;
+
+// Tipo das mensagens (5 bits)
+#define ACK 0 // bx00000
+#define NACK 1 // bx00001
+#define LISTAR 10 // bx01010
+#define BAIXAR 11 // bx01011
+#define PRINTAR 16 // bx10000
+#define DESCRITOR 17 // bx10001
+#define DADOS 18 // bx10010
+#define FIM 30 // bx11110
+#define ERRO 31 // bx11111
+
+
+#endif // SOCKET_H
