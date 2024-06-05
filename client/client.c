4dd110b380ca2d97d50d983fcf1f8f62c0a63423
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-06-04 21:11:07 -0300

    [wip] trying to open and connect a raw socket

--- client/client.c
+++ client/client.c
@@ -3,5 +3,12 @@
 int main(int argc, char *argv[]) {
     int raw_socket;
 
-    raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL);
+
+    if (raw_socket < 0) {
+        perror("socket()");
+        return -1;
+    }
+
+
 }
