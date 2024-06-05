2139121a4f415472ef199569876dd911852cba7f
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-06-04 22:33:41 -0300

    [created] utils file [add] crc8 method option

---
+++ utils/utils.h
@@ -0,0 +1,14 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
+#include <linux/if.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#endif // __UTILS_H__
