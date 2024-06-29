f7498b6b5e035903128c8c2f5b693741f63c8b05
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-06-29 14:38:06 -0300

    [add] init_client function

--- client/client.c
+++ client/client.c
@@ -1,43 +1,33 @@
 #include "client.h"
 
 
-struct sockaddr socket_address;
-struct sockaddr_in source,dest;
-unsigned char *send_buffer;
+connection_t connection;
+
+void init_client(char *interface){
+    connection.state = SENDING;
+    connection.socket = ConexaoRawSocket(interface);
+
+    // Destination MAC address (example, use the actual destination address)
+    uint8_t dest_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Broadcast address for example
+
+    // Prepare sockaddr_ll structure
+    memset(&connection.address, 0, sizeof(connection.address));
+    connection.address.sll_family = AF_PACKET;
+    connection.address.sll_protocol = htons(ETH_P_ALL);
+    connection.address.sll_ifindex = if_nametoindex(interface);
+    connection.address.sll_halen = ETH_ALEN;
+    memcpy(connection.address.sll_addr, dest_mac, ETH_ALEN);
+}
 
 int main() {
-    const char *interface = "enp6s0";
-    int raw_socket = create_stream_socket(0);
+    init_client("lo");
 
-    if (raw_socket < 0) {
-        fprintf(stderr, "Failed to create and configure raw socket on interface %s\n", interface);
-        return EXIT_FAILURE;
+    while (1)
+    {
+        packet_t pkt;
+        build_packet(&pkt, 0, 0, "Hello, world!", strlen("Hello, world!"));
+        send_packet(connection.socket, &pkt, &connection.address);
     }
-
-    packet_t packet;
-    struct sockaddr_ll src_addr;
-    socklen_t src_addr_len = sizeof(src_addr);
-
-    while (1) {
-        ssize_t num_bytes = recvfrom(raw_socket, &packet, sizeof(packet), 0, (struct sockaddr *)&src_addr, &src_addr_len);
-        if (num_bytes < 0) {
-            perror("recvfrom");
-            close(raw_socket);
-            return EXIT_FAILURE;
-        }
-
-        uint8_t calculated_crc = calculate_crc8((uint8_t *)&packet, sizeof(packet) - 1);
-        if (packet.starter_mark == 0xAA) {
-            printf("Received valid packet: ");
-            for (int i = 0; i < packet.size; ++i) {
-                printf("%c", packet.data[i]);
-            }
-            printf("\n");
-        } else {
-            printf("Received invalid packet\n");
-            print_packet(&packet);
-        }
-   }
 
     return EXIT_SUCCESS;
 }
