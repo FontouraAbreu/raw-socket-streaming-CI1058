8dba5cd355a8ceae664ad2d18ba0edbd8430cf9c
simon <gabriel.ribeiro@smartinnovation.com.br>
2024-08-09 15:58:54 -0300

    [feat] update video download path in client.c

--- client/client.c
+++ client/client.c
@@ -2,7 +2,7 @@
 
 connection_t connection;
 
-#define VIDEO_CLIENT_LOCATION "./videos_client/testevideo.mp4"
+#define VIDEO_CLIENT_LOCATION "./videos_client/"
 
 typedef enum
 {
@@ -110,7 +110,10 @@ int main(int argc, char **argv)
 
             video_t chosen_video = video_list->videos[chosen_video_index - 1];
             char *video_path = malloc(strlen(VIDEO_CLIENT_LOCATION) + strlen(chosen_video.name) + 1);
-            printf("Baixando video %s\n", chosen_video.name);
+            strcpy(video_path, VIDEO_CLIENT_LOCATION);
+            strcat(video_path, chosen_video.name);
+            printf("Baixando video %s\n", video_path);
+
 
             request_download(chosen_video.name);
 
@@ -122,7 +125,7 @@ int main(int argc, char **argv)
             if (packet.type == INICIO_SEQ)
             {
                 printf("Recebendo dados do video...\n");
-                receive_video_packet_sequence(connection.socket, &packet, &connection, VIDEO_CLIENT_LOCATION, chosen_video.size);
+                receive_video_packet_sequence(connection.socket, &packet, &connection, video_path, chosen_video.size);
             }
 
             break;
